97c1a857c2c279abc42391aa4f03ba21584b3b0c
Glenn Aarøen <glaar90@gmail.com>
2015-03-18 13:33:38 +0100

    add log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   root 'home#index'
   get 'home/index'
 
   get 'users/new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
